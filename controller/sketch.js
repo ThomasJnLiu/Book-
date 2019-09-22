31544c78a24b5671beadb6522bb4438fd5ac2230
Thomas <thomasjnliu@gmail.com>
2019-09-21 20:02:03 -0400

    add generation of UUID

--- controller/sketch.js
+++ controller/sketch.js
@@ -4,6 +4,11 @@ let subKey = 'sub-c-4d640c3e-d831-11e9-85e7-eae1db32c94a';
 
 let channelName = "whoSaysStuff";
 
+/*number for identifying players
+1-3 are players, 0 is for spectators when there are more than 3 people subscribed.*/
+let playerNum;
+//universally unique identifier, used to differentiate users
+let newUUID = PubNub.generateUUID();
 
 function setup() 
 {
@@ -13,11 +18,12 @@ function setup()
    // initialize pubnub
   dataServer = new PubNub(
   {
+    uuid: newUUID,
     publish_key   : pubKey,  //get these from the pubnub account online
     subscribe_key : subKey,  
     ssl: true  //enables a secure connection. This option has to be used if using the OCAD webspace
   });
-  
+  console.log(newUUID);
   //attach callbacks to the pubnub object to handle messages and connections
   dataServer.addListener({message: readIncoming, presence: presenceChange});
   dataServer.subscribe({channels: [channelName], withPresence: true});
@@ -26,6 +32,23 @@ function setup()
 function presenceChange(pInfo){
     console.log(pInfo.occupancy);
     totalPopulation = pInfo.occupancy;
+
+    //determine player number based on # of users subscribed
+    switch(pInfo.occupancy){
+      case 2:
+        playerNum = 1;
+        break;
+      case 3: 
+        playerNum = 2;
+        break;
+      case 4:
+        playerNum = 3;
+        break;
+      default:
+        playerNum = 0;
+        break;
+    }
+    //console.log('player number is ' + playerNum);
 }
 
   function readIncoming(inMessage) //when new data comes in it triggers this function, 
